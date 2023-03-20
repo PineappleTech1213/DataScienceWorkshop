1d46cbd5e86a6fa2ef6fff0fc8f1e5409dfe2507
PineappleTech <125321926+PineappleTech1213@users.noreply.github.com>
2023-03-20 18:22:11 -0400

    Add files via upload

---
+++ DistributedSystem/getEmoji/app/src/main/java/cmu/edu/zhengzen/getemoji/Emoji.java
@@ -0,0 +1,25 @@
+package cmu.edu.zhengzen.getemoji;
+
+/**
+ * Email: zhengzen@andrew.cmu.edu
+ * Author: Zheng Zeng
+ * This is the class that store information about an emoji that needs to be sent to the client
+ * no additional info rather than the image url needs to be sent.
+ **/
+public class Emoji {
+    //the url of the emoji image, ends in .png
+    String image_url;
+
+    //constructor
+    public Emoji(String url){
+        setImage_url(url);
+    }
+    //setter and getter
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+}
